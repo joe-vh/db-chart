18210f25b77f5d3d7851f4c5dbedcc881794a910
joe-vh <joevanhien@gmail.com>
2017-06-18 01:14:38 +0200

    Update Grid.js

--- charts/BarChart/Grid.js
+++ charts/BarChart/Grid.js
@@ -1,37 +1,51 @@
-import React, { Component, PropTypes } from 'react'
-import { Motion, spring } from 'react-motion'
+import React, {Component, PropTypes} from 'react'
+import {Motion, spring} from 'react-motion'
 
 class Grid extends Component {
-  render () {
+  render() {
+    const {
+      values, xSpacing, ySpacing, yModulus, chartWidth,
+      yAxis, topPadding, chartHeight,
+    } = this.props
 
-    const { values, xSpacing, ySpacing, yModulus, chartWidth, yAxis, topPadding, chartHeight } = this.props
-    
     return (
-      <g transform={'translate(51,'+topPadding+')'}>
+      <g
+        transform={`translate(51, ${(topPadding)})`}
+      >
         {
-          values.map((v, i) => (
-            <rect
-              x={i*xSpacing}
-              y={0}
-              key={i}
-              width='1'
-              height={chartHeight - ySpacing}
-              style={{ fill: '#efefef' }}
-              rx='2'
-            />
-          ))
+          values.map(
+            (v, i) => (
+              <rect
+                x={i * xSpacing}
+                y={0}
+                key={i}
+                width="1"
+                height={chartHeight - ySpacing}
+                style={{fill: '#efefef'}}
+                rx="2"
+              />
+            )
+          )
         }
         {
           yAxis.map((y, i) => {
-            if (y % yModulus === 0) return <rect
-              x={0}
-              y={i*ySpacing}
-              key={i}
-              width={chartWidth - xSpacing}
-              height={1}
-              style={{ fill: i === 0 || i === yAxis.length - 1 || y === 0 ? '#acacac' : '#efefef' }}
-              rx='2'
-            />
+            if (y % yModulus === 0) {
+              const isDivision = i === 0 || i === yAxis.length - 1 || y === 0
+
+              return (
+                <rect
+                  x={0}
+                  y={i * ySpacing}
+                  key={i}
+                  width={chartWidth - xSpacing}
+                  height={1}
+                  style={{
+                    fill: isDivision ? '#acacac' : '#efefef',
+                  }}
+                  rx="2"
+                />
+              )
+            }
           })
         }
       </g>

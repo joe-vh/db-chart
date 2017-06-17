244afcc9e926e35ba869083cebddf1c5c994aeb0
joe-vh <joevanhien@gmail.com>
2017-06-18 01:13:55 +0200

    Update AxisY.js

--- charts/BarChart/AxisY.js
+++ charts/BarChart/AxisY.js
@@ -1,19 +1,37 @@
-import React, { Component, PropTypes } from 'react'
-import { Motion, spring } from 'react-motion'
+import React, {Component, PropTypes} from 'react'
+import {Motion, spring} from 'react-motion'
 
 class AxisY extends Component {
-  render () {
+  render() {
+    const {
+      yAxis, ySpacing, yTitle, yUnit, chartHeight, yModulus, topPadding,
+    } = this.props
 
-    const { yAxis, ySpacing, yTitle, yUnit, chartHeight, yModulus, topPadding } = this.props
-    
     return (
-      <g transform={'translate(0,'+topPadding+')'}>
+      <g
+        transform={`translate(0, ${topPadding})`}
+        >
         {
           yAxis.map((y, i) => {
-            if (y % yModulus === 0) return <text x='5' y={i*ySpacing + 5} key={i}>{y+yUnit}</text>
+            if (y % yModulus === 0) {
+              return (
+                <text x="5" y={i * ySpacing + 5} key={i}>
+                  {y+yUnit}
+                </text>
+              )
+            }
           })
         }
-        { yTitle && <text x={0} y={chartHeight/2 - (5*yTitle.length)}>{yTitle}</text> }
+        {
+          yTitle && (
+            <text
+              x={0}
+              y={chartHeight / 2 - (5 * yTitle.length)}
+              >
+              {yTitle}
+            </text>
+          )
+        }
       </g>
     )
   }

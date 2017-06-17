ccee2d8f59f236a490953def29620ef442bda5f1
joe-vh <joevanhien@gmail.com>
2017-06-18 01:13:31 +0200

    Update AxisX.js

--- charts/BarChart/AxisX.js
+++ charts/BarChart/AxisX.js
@@ -1,21 +1,40 @@
-import React, { Component, PropTypes } from 'react'
-import { Motion, spring } from 'react-motion'
+import React, {Component, PropTypes} from 'react'
+import {Motion, spring} from 'react-motion'
 
 class AxisX extends Component {
-  render () {
+  render() {
+    const {
+      values, chartHeight, chartWidth, xTitle, xSpacing, topPadding, xModulus,
+    } = this.props
 
-    const { values, chartHeight, chartWidth, xTitle, xSpacing, topPadding, xModulus } = this.props
-    
     return (
-      <g transform={'translate(51,'+(topPadding + chartHeight)+')'}>
+      <g
+        transform={`translate(51, ${(topPadding + chartHeight)})`}
+      >
         {
           values.map((v, i) => {
-            if (i % xModulus === 0) return <text x={i*xSpacing - 15} y={15} key={i}>
-              {v.xValue.getMonth ? v.xValue.getMonth()+'.'+v.xValue.getFullYear() : v.xValue}
-            </text>
+            if (i % xModulus === 0) {
+              return (
+                <text x={i * xSpacing - 15} y={15} key={i}>
+                  {
+                    v.xValue.getMonth
+                    ? `${v.xValue.getMonth()}.${v.xValue.getFullYear()}`
+                    : v.xValue
+                  }
+                </text>
+              )
+            }
           })
         }
-        { xTitle && <text x={((values.length-1)*xSpacing)/2 - (xTitle.length*4)} y={40}>{xTitle}</text> }
+        {
+          xTitle && (
+            <text
+              x={((values.length - 1) * xSpacing) / 2 - (xTitle.length * 4)}
+              y={40}
+            >
+              {xTitle}
+            </text>
+          )}
       </g>
     )
   }
